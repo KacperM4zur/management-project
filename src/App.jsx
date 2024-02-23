102de33b946704ffcc20c596b9c4b3016c8bd208
Kacper <kacper.mazur4@proton.me>
2024-02-23 23:58:42 +0100

    menu bar, main page, add task form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
+   const [projectsState, setProjectState] = useState({
+       selectedProjectId: undefined,
+       projects: []
+   });
+
+   function handleStartAddProject() {
+       setProjectState(prevState => {
+           return {
+               ...prevState,
+               selectedProjectId: null,
+           };
+       });
+   }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar/>
+    <main className="h-screen my-8 flex gap-8">
+       <ProjectsSidebar/>
+       <NoProjectSelected/>
     </main>
   );
 }
